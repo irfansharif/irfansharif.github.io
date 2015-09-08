aa2fca8242ed7b3533720307a1ad2b4f7e139566
Irfan Sharif <irfanmms@hotmail.com>
2015-09-07 23:46:21 -0400

    updated release date

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "September 20, 2015 18:03:26",
+	date: "September 30, 2015 18:03:26",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
